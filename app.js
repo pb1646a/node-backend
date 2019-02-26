7792fd3dfdaa57a57df9d6216898e3c235cf65c5
pb1646a <pb1646a@gmail.com>
2019-02-26 10:50:39 -0500

    added sine changes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const dbUrl = process.env.DB_URI;
 mongoose.set('useCreateIndex', true);
-
 mongoose
   .connect(
     dbUrl,
@@ -14,7 +13,7 @@ mongoose
     console.log("connected to database!");
   })
   .catch(() => {
-    console.log("connection failed");
+    console.log("connection to the db failed");
   });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +22,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-requested-With, Content-Type, Accept"
+    "Origin, X-requested-With, Content-Type, Accept, Authorization"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
